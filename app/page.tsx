37b8aae64d72914656bad993dc0d476529c0ee59
anthonysuarez <antsuarez305@gmail.com>
2023-10-27 18:57:26 -0500

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button'
+
+const state = true
+
+export default function Home() {
+  return (
+    <div className=''>
+      <p className='text-3xl font-bold text-indigo-500'>Hello Discord Clone!</p>
+      <Button>Click me</Button>
+    </div>
+  )
+}
