985d69344c2769fc6c0185cb34d4f8ba9da95f6b
anthonysuarez <antsuarez305@gmail.com>
2023-10-27 19:35:35 -0500

    Set Up Authentication

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,5 +1,11 @@
+import { UserButton } from '@clerk/nextjs'
+
 const state = true
 
 export default function Home() {
-  return <div>This is a protected route</div>
+  return (
+    <div>
+      <UserButton afterSignOutUrl='/' />
+    </div>
+  )
 }
